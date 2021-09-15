064d50b86589c17f0e169f46952ab9608375fb2e
Joels17 <joel.semeniuk@gmail.com>
2021-09-15 16:34:00 -0700

    Initial project setup

---
+++ COSC499_demoProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace COSC499_demoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert a string: ");
+            var input = Console.ReadLine();
+
+            var stringMan = new StringManipulator();
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ COSC499_demoProject/StringManipulator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSC499_demoProject
+{
+    public class StringManipulator
+    {
+    }
+}
